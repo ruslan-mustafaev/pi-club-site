941c1ee93cbb0e5f48b638f54e3f04334c8edd45
ruslan-mustafaev <therox5551998@gmail.com>
2026-05-30 16:59:36 +0200

    Fix image paths in frontend files

--- script.js
+++ script.js
@@ -42,11 +42,9 @@ const zoneData = {
     title: "Billiard Lounge",
     text: "Komfortná billiard lounge zóna s tlmeným osvetlením, príjemnou hudbou, prémiovým barom a atmosférou pre oddych počas týždňa.",
     images: [
-      { src: "/img/biliard/b0.jpg", label: "Billiard Lounge" },
-      { src: "/img/biliard/b1.jpg", label: "Billiard Lounge" },
-      { src: "/img/biliard/b2.jpg", label: "Billiard Lounge" },
-      { src: "/img/biliard/b3.jpg", label: "Billiard Lounge" },
-      { src: "/img/biliard/b4.jpg", label: "Billiard Lounge" },
+      { src: "/billiard.jpg", label: "Billiard Lounge" },
+      { src: "/billiard-uNajkgG4.jpg", label: "Billiard Lounge" },
+      { src: "/b1-TSW9RQO3.jpg", label: "Billiard Lounge" },
     ],
   },
 
@@ -55,7 +53,8 @@ const zoneData = {
     title: "Signature Bar",
     text: "Dizajnový bar s prémiovou atmosférou, signature drinkami a priestorom pre štart večera pred klubovou nocou.",
     images: [
-      { src: "/img/bar/bar0.jpg", label: "Premium Bar" },
+      { src: "/bar.jpg", label: "Premium Bar" },
+      { src: "/bar-C-ZyiAkD.jpg", label: "Premium Bar" },
     ],
   },
 
@@ -64,8 +63,9 @@ const zoneData = {
     title: "VIP Karaoke 1",
     text: "Súkromná karaoke zóna pre spev, oslavy, menšie skupiny a VIP večery s vlastnou atmosférou.",
     images: [
-      { src: "/img/vip_karaoke_1/k0.jpg", label: "VIP Karaoke 1" },
-      { src: "/img/vip_karaoke_1/k2.jpg", label: "VIP Karaoke 1" },
+      { src: "/k0.jpg", label: "VIP Karaoke 1" },
+      { src: "/k0-hvGDKtSf.jpg", label: "VIP Karaoke 1" },
+      { src: "/k2.jpg", label: "VIP Karaoke 1" },
     ],
   },
 
@@ -74,8 +74,7 @@ const zoneData = {
     title: "VIP Karaoke 2",
     text: "Druhá karaoke zóna pre skupiny hostí, súkromné rezervácie, oslavy a večerný program.",
     images: [
-      { src: "/img/vip_karaoke_2/k2_1.jpg", label: "VIP Karaoke 2" },
-      { src: "/img/vip_karaoke_2/k2_2.jpg", label: "VIP Karaoke 2" },
+      { src: "/karaoke.jpg", label: "VIP Karaoke 2" },
     ],
   },
 
@@ -84,18 +83,11 @@ const zoneData = {
     title: "Dance Floor",
     text: "Energetická klubová zóna pre piatkové a sobotné noci, DJ program, svetelnú show a tanečný parket.",
     images: [
-      { src: "/img/disco_zone/d1.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d2.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d3.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d4.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d5.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d6.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d7.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d8.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d9.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d10.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d11.jpg", label: "Dance & Night" },
-      { src: "/img/disco_zone/d12.jpg", label: "Dance & Night" },
+      { src: "/led.jpg", label: "Dance & Night" },
+      { src: "/led-DAqHPzlF.jpg", label: "Dance & Night" },
+      { src: "/d3-dL6iKa3O.jpg", label: "Dance & Night" },
+      { src: "/stairs.jpg", label: "Dance & Night" },
+      { src: "/stairs-D5vmLmKd.jpg", label: "Dance & Night" },
     ],
   },
 };
